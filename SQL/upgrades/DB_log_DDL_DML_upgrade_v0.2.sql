2b7ce2a23de14fedd01265b8a10d7b98c599856d
Jesse Abdul <jesse.abdul@noaa.gov>
2020-07-08 15:24:22 -1000

    --Database Logging Module: Database Update--
    (this version defines and uses version 0.2 of the database)
    
    Database Updates: Increased the size of DB_LOG_ENTRIES.LOG_SOURCE to accommodate additional use cases.  Recompiled the dependent view DB_LOG_ENTRIES_V
    
    The database upgrade code was compiled in DB_log\SQL\upgrades\DB_log_DDL_DML_upgrade_v0.2.sql
    
    DB_log\SQL\DB_log_combined_DDL_DML.sql was updated to include the new version of the DB logging database
    
    The database documentation (DB_log\docs\Database Logging Module Documentation.docx) was updated to increment the version number

--- SQL/upgrades/DB_log_DDL_DML_upgrade_v0.2.sql
+++ SQL/upgrades/DB_log_DDL_DML_upgrade_v0.2.sql
@@ -7,9 +7,13 @@
 
 
 --------------------------------------------------------
---Database Log - Combined DDL/DML file:
+--Database Log - version 0.2 updates:
 --------------------------------------------------------
 
---add each database upgrade file in sequential order here:
-@@"./upgrades/DB_log_DDL_DML_upgrade_v0.1.sql"
-@@"./upgrades/DB_log_DDL_DML_upgrade_v0.2.sql"
+ALTER TABLE DB_LOG_ENTRIES
+MODIFY (LOG_SOURCE VARCHAR2(2000 BYTE) );
+
+ALTER VIEW DB_LOG_ENTRIES_V COMPILE;
+
+--define the upgrade version in the database upgrade log table:
+INSERT INTO DB_UPGRADE_LOGS (UPGRADE_APP_NAME, UPGRADE_VERSION, UPGRADE_DATE, UPGRADE_DESC) VALUES ('Database Log', '0.2', TO_DATE('08-JUL-20', 'DD-MON-YY'), 'Increased the size of DB_LOG_ENTRIES.LOG_SOURCE to accommodate additional use cases.  Recompiled the dependent view DB_LOG_ENTRIES_V');
